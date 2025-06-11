74b674b101e42cdb6e4757ad99e7a83a783555cc
Syed-FarooqGiga <motivationaldost@gmail.com>
2025-06-11 22:09:21 +0530

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -68,4 +68,4 @@ export const PERSONALITIES: Record<string, Personality> = {
   }
 };
 
-export const API_URL = 'http://82.29.162.54:8000';
+export const API_URL = 'https://https://api.gigabhai.com';
